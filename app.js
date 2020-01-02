e584b65321617774ae2d1132902515f837d51dfb
오경환 <32234263+rudghks373@users.noreply.github.com>
2020-01-02 11:46:01 +0900

    db connect

---
+++ app.js
@@ -0,0 +1,50 @@
+/* =======================
+    LOAD THE DEPENDENCIES
+==========================*/
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+/* =======================
+    LOAD THE CONFIG
+==========================*/
+const config = require("./config");
+const port = process.env.PORT || 3000;
+
+/* =======================
+    EXPRESS CONFIGURATION
+==========================*/
+const app = express();
+
+// parse JSON and url-encoded query
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// print the request log on console
+app.use(morgan("dev"));
+
+// set the secret key variable for jwt
+app.set("jwt-secret", config.secret);
+
+// index page, just for testing
+app.get("/", (req, res) => {
+  res.send("Hello JWT");
+});
+
+// open the server
+app.listen(port, () => {
+  console.log(`Express is running on port ${port}`);
+});
+
+/* =======================
+    CONNECT TO MONGODB SERVER
+==========================*/
+mongoose
+  .connect(
+    config.mongodbUri,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
+  .catch(error => console.log("connected to mongodb server error"))
+  .then(console.log("connected to mongodb server"));
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: "SeCrEtKeYfOrHaShInG",
+  mongodbUri:
+    "mongodb+srv://root:skejahf1@cluster0-ivryv.mongodb.net/test?retryWrites=true&w=majority"
+};
