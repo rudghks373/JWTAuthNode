16a6a828a93e088d5f373291f2058a7f1cf60b36
오경환 <32234263+rudghks373@users.noreply.github.com>
2020-01-06 11:15:32 +0900

    update

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -1,4 +1,5 @@
 const User = require("../../../models/user");
+const jwt = require("jsonwebtoken");
 
 /*
     POST /api/auth
@@ -59,4 +60,60 @@ exports.register = (req, res) => {
     .then(assign)
     .then(respond)
     .catch(onError);
+};
+
+/*
+    POST /api/auth/login
+    {
+        username,
+        password
+    }
+*/
+
+exports.login = (req, res) => {
+  res.send("login api is working");
+};
+
+/*
+    GET /api/auth/check
+*/
+
+exports.check = (req, res) => {
+  // read the token from header or url
+  const token = req.headers["x-access-token"] || req.query.token;
+
+  // token does not exist
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: "not logged in"
+    });
+  }
+
+  // create a promise that decodes the token
+  const p = new Promise((resolve, reject) => {
+    jwt.verify(token, req.app.get("jwt-secret"), (err, decoded) => {
+      if (err) reject(err);
+      resolve(decoded);
+    });
+  });
+
+  // if token is valid, it will respond with its info
+  const respond = token => {
+    res.json({
+      success: true,
+      info: token
+    });
+  };
+
+  // if it has failed to verify, it will return an error message
+  const onError = error => {
+    res.status(403).json({
+      success: false,
+      message: error.message
+    });
+  };
+
+  // process the promise
+  p.then(respond).catch(onError);
 };
